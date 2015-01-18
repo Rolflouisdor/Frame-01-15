127ae7072c9ea3ffdb51e2d1327b16a9c3912ff9
ROLF LOUISDOR <rolflouisdor@icloud.com>
2015-01-18 11:14:47 -0500

    Update function.js
    I completely change my logic for this next and previews function by adding another counter that keeps tracks when the program starts

--- Resources/function.js
+++ Resources/function.js
@@ -1,26 +1,44 @@
 var info =["List of names","Parlo","Jessica","Helmson","Hana","Jason","Watson"];
 
 var counter=0;
-
+var programHasStarted = false;
 var next = function()
 {
-	
+	if (programHasStarted == false) {
+		
+		counter=0;  
+		
+	}else{ 
+			if (counter === info.length-1){
+					counter=0;  
+				}else
+				{
+				   counter=counter+1;
+	            }
+	    }
 	displayInfo.text = info[counter];
-	if (counter === info.length-1){
-    counter=0;  
-}else{
-		counter=counter+1;
-}};
+	programHasStarted = true;
+	
+};
 
 var Previous = function(){
-    displayInfo.text = info[counter];
+
+	if (programHasStarted == false) {
+		
+		counter=0;  
+		
+	}else{  
     if (counter === 0){
     counter=info.length-1;
-}else{
-counter=counter-1;
-}};
+	}else{
+	counter=counter-1;
+}}
+    displayInfo.text = info[counter];    
+    programHasStarted = true;
+};
 
 
 nextButton.addEventListener("click",next);
 
 previousButton.addEventListener("click",Previous);
+
